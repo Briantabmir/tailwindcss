661f06aa21d5466adaaf86a67efc42469516ad82
Brian Tablit Miranda <briantabmir@campus.monlau.com>
2023-12-19 19:10:00 +0100

    subida de archivos

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}", // depende de la estructura que eliges
+    "./src/**/*.{html,js}" // en este caso hay dos carpetas: public / scr
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
